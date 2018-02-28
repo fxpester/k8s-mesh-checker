e677b4f9628f7704f4a8a43cad8f0f0c05abc2e0
ayurtaykin <a.yurtaykin@gmail.com>
2018-02-28 17:20:43 +0300

    first commit

---
+++ check-network.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+        echo "Usage: $0 port ## checks port for fullmesh connectivity in k8s cluster"
+        exit
+fi
+
+
+
+cat <<EOF3 | kubectl create -f -
+apiVersion: extensions/v1beta1
+kind: DaemonSet
+metadata:
+  name: echoserver
+spec:
+  template:
+    metadata:
+      labels:
+        name: echoserver
+    spec:
+      tolerations:
+        - key: "node-role.kubernetes.io/master"
+          operator: "Exists"
+          effect: "NoSchedule" 
+      containers:
+        - image: centos
+          name: centos
+          command: ["/bin/sh"]
+          args: ["-c", "yum install nmap-ncat-6.40-7.el7.x86_64 -y && nc -l -k -n -p $1"]
+          imagePullPolicy: Always
+
+
+EOF3
+	
+	
+	
+
+
+
+
+cat <<EOF2 | kubectl create -f -
+apiVersion: extensions/v1beta1
+kind: DaemonSet
+metadata:
+  name: echoserver-hostnetwork
+spec:
+  template:
+    metadata:
+      labels:
+        name: echoserver-hostnetwork
+    spec:
+      tolerations:
+        - key: "node-role.kubernetes.io/master"
+          operator: "Exists"
+          effect: "NoSchedule" 
+      hostNetwork: true
+      containers:
+        - image: centos
+          name: centos
+          command: ["/bin/sh"]
+          args: ["-c", "yum install nmap-ncat-6.40-7.el7.x86_64 -y && nc -l -k -n -p $1"]
+          imagePullPolicy: Always
+          securityContext:
+            privileged: true
+
+
+EOF2
+
+
+
+
+sleep 120 
+
+
+export failures=''
+hosts=$(kubectl get pod -o wide | grep echoserver | awk '{print $6}')
+kubehosts=$(kubectl get pod -o wide | grep echoserver | awk '{print $1}')
+
+
+for host in $hosts
+do
+
+ for kubehost in $kubehosts
+ do
+ echo "checking pod - $kubehost connection to $host"
+ kubectl exec $kubehost -- echo dummy-payload  > /dev/tcp/$host/$1 || failures="$failures pod - $kubehost cant connect to $host"
+ done
+
+done
+
+
+
+echo "failed hosts: $failures"
+
+
+
+
+kubectl delete ds echoserver-hostnetwork
+kubectl delete ds echoserver
+
+
+
