b08d21ef4760429ce1fa6277310c7e072fe7a76d
Andrey Yurtaykin <7628763+fxpester@users.noreply.github.com>
2018-03-07 15:50:21 +0300

    Update check-network.sh

--- check-network.sh
+++ check-network.sh
@@ -102,6 +102,9 @@ do
  node=$(kubectl get pod -o wide | grep $ip | awk '{print $7}')
  kubectl exec $pod -- curl --retry 2 --connect-timeout 2 -f -s -o /dev/null ${ip}:${1} || failures="$failures \n pod - $pod cant connect to svc at $svc:${1} running on node $node"
  done
+ 
+ pod-node=$(kubectl get pod -o wide | grep $pod | awk '{print $7}')
+ kubectl exec $pod -- curl --retry 2 --connect-timeout 2 -f -s -o /dev/null https://api.telegram.org/ || failures="$failures \n pod - $pod running on $pod-node cant connect to telegram"    
 
 done
 
